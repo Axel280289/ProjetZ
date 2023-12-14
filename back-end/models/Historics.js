ad1942ca58861521b195f01efcd1193076c2091e
Lulu <ludivinevaillant@outlook.fr>
2023-12-14 11:03:46 +0100

    final rebase

--- back-end/models/Historics.js
+++ back-end/models/Historics.js
@@ -10,15 +10,3 @@ const historicSchema = mongoose.Schema({
 historicSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('Historics', historicSchema, 'historics');
-const mongoose= require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
-
-const historicSchema = mongoose.Schema({
-    date: {type: Date, required: true},
-    amount: {type: Number, required: true},
-    associations: {type: [String], required: true, trim: true},
-});
-
-historicSchema.plugin(uniqueValidator);
-
-module.exports = mongoose.model('Historics', historicSchema, 'historics');
